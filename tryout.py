a7884304673417259999f89e84eaafb1d2be5760
Ile Eftimov <eftimov2@gmail.com>
2011-09-09 20:09:41 +0200

    Added CLI arguments functionality.

--- tryout.py
+++ tryout.py
@@ -18,6 +18,7 @@ def storeToDB(feeds):
     pickle.dump(feeds, data_file)
   print("All RSS Feeds stored!")    
 
+
 def loadFromDB():
   '''A function that retrieves all the feeds urls from a file on disk, and returns it.'''
   with open("feedurls.data", "rb") as data_file:
@@ -26,7 +27,7 @@ def loadFromDB():
   
     
 def getRSS(feed_urls):
-  '''A function that returns an array containing all entries for all the feed urls in feedurls.data file (date of creation, the title and the link)'''
+  '''A function that returns an array containing all entries for all the feed urls in feedurls.data file (date of creation, the title and the URL)'''
   feed_results = {}
   for url in feed_urls: 
     tmp = feedparser.parse(url)
@@ -37,10 +38,18 @@ def getRSS(feed_urls):
   return feed_results
 
 
+def printResults(feed_results):
+  for feed_name, entries in feed_results.items():
+    print(120*"=")
+    print("Feed for: ",feed_name)
+    print(120*"=")
+    for entry in entries:
+      print("[Posted:] {0}\t[Title:] {1}\t[URL:] {2}".format(time.asctime(entry[0]), entry[1], entry[2]))
+
 def printMenu():
-  print("="*80)
+  print("="*120)
   print("Welcome to my console RSS reader!")
-  print("="*80)
+  print("="*120)
   print("1. Enter a new feed url")
   print("2. Read RSS")
   print("3. Delete feed url")
@@ -48,48 +57,65 @@ def printMenu():
   print("="*80)
 
 
+
+def invokeMenu(feed_urls):
+  results = {}
+  while True:
+    printMenu()
+    try:
+      menu_input = int(input("Choose an option [1, 2, 3, 4]: "))
+    except ValueError:
+      print("No such option, please try again.")
+      menu_input = int(input("Choose an option [1, 2, 3, 4]: "))
+    except KeyboardInterrupt:
+      sys.exit("Goodbye!")
+
+    if menu_input == 4:
+      storeToDB(feed_urls)
+      sys.exit("Goodbye!")
+
+    elif menu_input == 1:
+      feed_url = str(input("Please enter the url to the RSS feed: "))
+      feed_urls.append(feed_url)
+      print("The Feed URL has been saved.")
+
+    elif menu_input == 2:
+      results = getRSS(feed_urls)
+      printResults(results)
+
+
+    elif menu_input == 3:
+      cnt = 0
+      for url in feed_urls:
+        print("{0}. {1}".format(cnt+1, url))
+        cnt += 1
+      del_choose = int(input("Enter the number of the feed you want to delete:"))
+      del feed_urls[del_choose-1]   
+
+ 
 try:
   feed_urls = loadFromDB()
 except EOFError:
   feed_urls = []
   print("No RSS Feed entries. Please add some first.")
 
-results = {}
-while True:
 
-  printMenu()
-  try:
-    menu_input = int(input("Choose an option [1, 2, 3, 4]: "))
-  except ValueError:
-    print("No such option, please try again.")
-    menu_input = int(input("Choose an option [1, 2, 3, 4]: "))
-  except KeyboardInterrupt:
-    sys.exit("Goodbye!")
-
-  if menu_input == 4:
-    storeToDB(feed_urls)
-    sys.exit("Goodbye!")
-
-  elif menu_input == 1:
-    feed_url = str(input("Please enter the url to the RSS feed: "))
-    feed_urls.append(feed_url)
-    print("The Feed URL has been saved.")
-
-  elif menu_input == 2:
-    results = getRSS(feed_urls)
-    cnt = 0
-    for feed_name, entries in results.items():
-      print(80*"=")
-      print("Feed for: ",feed_name)
-      print(80*"=")
-      cnt += 1
-      for entry in entries:
-        print("[Posted:] {0}\t[Title:] {1}\t[URL:] {2}".format(time.asctime(entry[0]), entry[1], entry[2]))
-
-  elif menu_input == 3:
-    cnt = 0
-    for url in feed_urls:
-      print("{0}. {1}".format(cnt+1, url))
-      cnt += 1
-    del_choose = int(input("Enter the number of the feed you want to delete:"))
-    del feed_urls[del_choose-1]    
+if len(sys.argv) < 2:
+  invokeMenu(feed_urls)
+elif sys.argv[1] == "-r":
+  results = {}
+  results = getRSS(feed_urls)
+  printResults(results)
+elif sys.argv[1] == "-a":
+  feed_urls.append(sys.argv[2])
+  print("URL: {0} added to database.".format(sys.argv[2]))
+  storeToDB(feed_urls)
+elif sys.argv[1] == "-d":
+  print("Not available yet!")
+elif sys.argv[1] == "--help" or sys.argv[1] == "-h":
+  print("Feedzor - a CLI RSS Reader")
+  print("Options:")
+  print("No Arguments : Loads an interactive menu.")
+  print("-r : Prints all RSS from DB")
+  print("-a <url to rss> : Adds an URL to the database.")
+  print("-d <url to rss> : Deletes that url from the database.")
